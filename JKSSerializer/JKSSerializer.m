74d82691c5646b8e45392ce45e4445f06ae3a3db
Jeff Hui <jeff@jeffhui.net>
2013-10-07 21:10:07 -0400

    Initial Commit

---
+++ JKSSerializer/JKSSerializer.m
@@ -0,0 +1,13 @@
+//
+//  JKSSerializer.m
+//  JKSSerializer
+//
+//  Created by Jeff Hui on 10/7/13.
+//  Copyright (c) 2013 Jeff Hui. All rights reserved.
+//
+
+#import "JKSSerializer.h"
+
+@implementation JKSSerializer
+
+@end
