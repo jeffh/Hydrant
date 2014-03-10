e1c3511b92d12c9d587fe5bb5eb6457bd1a4757d
Jeff Hui <jeff@jeffhui.net>
2014-03-09 20:11:18 -0700

    Bad typos.

--- Hydrant/Public/Mappers/Core/HYDForwardMapper.h
+++ Hydrant/Public/Mappers/Core/HYDForwardMapper.h
@@ -10,11 +10,11 @@
 @end
 
 HYD_EXTERN_OVERLOADED
-id<HYDMapper> HYDMapForward(id<HYDAccessor> accessor, Class destinationClass, id<HYDMapper> childMapper)
+id<HYDMapper> HYDMapForward(id<HYDAccessor> accessor, Class sourceClass, id<HYDMapper> childMapper)
 HYD_REQUIRE_NON_NIL(1,2,3);
 
 HYD_EXTERN_OVERLOADED
-id<HYDMapper> HYDMapForward(NSString *walkKey, Class destinationClass, id<HYDMapper> childMapper)
+id<HYDMapper> HYDMapForward(NSString *walkKey, Class sourceClass, id<HYDMapper> childMapper)
 HYD_REQUIRE_NON_NIL(1,2,3);
 
 HYD_EXTERN_OVERLOADED
